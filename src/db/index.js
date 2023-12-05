e1335654772d64d2c7f4ff5b447bbef3a458f26f
Neer Khandor <neerkhandor456@gmail.com>
2023-12-05 18:27:13 +0530

    Connect to Mongo Atlas

--- src/db/index.js
+++ src/db/index.js
@@ -1,2 +1,22 @@
 import { mongoose } from "mongoose"
-import DB_NAME from '../constant'
+import DB_NAME from '../constant.js'
+// nodejs gives access to process
+// ye process hai na currrent application run horahi hogi uska reference hai
+// mongoose aapko ek return object deta hai uss object ko aapko jo naam dena ho dedo
+// and wo usko handle karlega
+// so jobhi response aa raha hai aap usse variable ke andar hold kar rahe ho
+
+
+const connectDB=async()=>{
+    try{
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\n MongoDB Connected !! db host : ${connectionInstance.connection.host}`);
+        // isse mujhe pata chalta hai ki mein konse host mein connect hua hu
+
+    }
+    catch(error){
+        console.log("MongoDb connection failed ",error);
+        process.exit(1)
+    }
+}
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import mongoose  from "mongoose";
-import { DB_NAME } from "./constant";
+//require('dotenv').config({path: './env'});
+import dotenv from "dotenv";
+// import mongoose  from "mongoose";
+// import { DB_NAME } from "./constant";
+// import express from "express" 
+import connectDB from "./db/index.js"
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+// 2nd approach alag se ek file le wahape saara code likhe waha se export karke index file mein import kar denge
+// we try that as early it is possible we load the environment variables
+
+/*
+// kai baar kya hota hai isi file mein express initialize kar lete hai
+// aap kya dikhega kai baar ki jaise hi connect ho chuka hia ki next line mein listeners laga sakte hai
+// ye ek acha method hai aapne pura apna db ka connection ek hi file mein likh 
+// rakha hai bahar kuch pollute nahi kiya hai but thoda index file zyada pollute hogaya hai
 
 ;(async ()=>{
     try{
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        
+        app.on("error",(error)=>{
+            console.log("ERR: ",error);
+            throw error;
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
     }
     catch(error){
         console.error("Error",error);
         throw error;
     }
 })()
+*/
